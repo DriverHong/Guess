57b75c24ea8ffcead2ea52d9b7872832e67782d4
Luka <270500328@qq.com>
2019-03-15 09:48:36 +0800

    注释

--- 猜数字/js/index.js
+++ 猜数字/js/index.js
@@ -10,17 +10,19 @@ window.onload = function () {
 
     let guessCount = 1;
     let resetButton;
-
+    /* 判断函数 */
     function CheckGuess() {
-        const userguess = Number(guessField.value);   //获取到用户输入的值，赋给userguess
+        //获取到用户输入的值，赋给userguess
+        const userguess = Number(guessField.value);
         if (guessCount == 1) {
             guesses.textContent = '您猜的数：';
         }
-        guesses.textContent += userguess + ' ';   //用户输入的数字显示到class为userguess的p标签
+        //用户输入的数字显示到class为userguess的p标签
+        guesses.textContent += userguess + ' ';
         if (userguess === randomNumber) {
             lastResult.textContent = 'Bingo！';
             lastResult.style.backgroundColor = 'green';
-            lowOrHi.textContent = '';   //清空
+            lowOrHi.textContent = '';
             setGameOver();
         } else if (guessCount == 10) {
             lastResult.textContent = 'GameOver';
@@ -35,37 +37,46 @@ window.onload = function () {
             }
         }
         guessCount++;
+        /* 猜完一次之后文本框清空 */
         guessField.value = '';
+        /* 光标自动选中文本框 */
         guessField.focus();
     }
+    /* 添加事件监听 */
     guessSubmit.addEventListener('click', CheckGuess);
 
+    /* 游戏结束函数 */
     function setGameOver() {
+        //游戏结束后通过disabled属性设置true来禁用文本框和按钮
         guessField.disabled = true;
         guessSubmit.disabled = true;
+        /* 创建一个按钮 */
         resetButton = document.createElement('button');
+        /* 设置按钮名称 */
         resetButton.textContent = '开始新游戏';
+        /* 向末端添加button */
         document.body.appendChild(resetButton);
+        /* 添加事件监听 */
         resetButton.addEventListener('click', resetGame);
     }
-
+    /* 游戏重新开始函数 */
     function resetGame() {
         guessCount = 1;
-      
+        /* 清空所有段落信息 */
         const resetParas = document.querySelectorAll('.resultParas p');
-        for (let i = 0 ; i < resetParas.length; i++) {
-          resetParas[i].textContent = '';
+        for (let i = 0; i < resetParas.length; i++) {
+            resetParas[i].textContent = '';
         }
-      
+        /* 删除重置按钮 */
         resetButton.parentNode.removeChild(resetButton);
-      
+        /* 文本框可用，按钮可用，并且光标自动选中文本框 */
         guessField.disabled = false;
         guessSubmit.disabled = false;
         guessField.value = '';
         guessField.focus();
-      
+        /* 清空lastResult背景颜色 */
         lastResult.style.backgroundColor = 'white';
-      
+        /* 重新生成一个随机数 */
         randomNumber = Math.floor(Math.random() * 100) + 1;
-      }
+    }
 }
